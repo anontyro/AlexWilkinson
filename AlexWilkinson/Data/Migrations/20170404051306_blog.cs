a460641f982accd9c4579b29e38c0f46f4303276
anontyro <alexwilkinson@gmail.com>
2017-04-04 13:48:43 +0800

    Add project files.

---
+++ AlexWilkinson/Data/Migrations/20170404051306_blog.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace AlexWilkinson.Data.Migrations
+{
+    public partial class blog : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUserRoles_UserId",
+                table: "AspNetUserRoles");
+
+            migrationBuilder.DropIndex(
+                name: "RoleNameIndex",
+                table: "AspNetRoles");
+
+            migrationBuilder.CreateTable(
+                name: "Blog",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Author = table.Column<string>(nullable: true),
+                    Body = table.Column<string>(nullable: true),
+                    CoverImg = table.Column<string>(nullable: true),
+                    Created = table.Column<DateTime>(nullable: false),
+                    Draft = table.Column<bool>(nullable: false),
+                    Published = table.Column<DateTime>(nullable: false),
+                    Title = table.Column<string>(nullable: true),
+                    UrlSlug = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Blog", x => x.ID);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "RoleNameIndex",
+                table: "AspNetRoles",
+                column: "NormalizedName",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Blog");
+
+            migrationBuilder.DropIndex(
+                name: "RoleNameIndex",
+                table: "AspNetRoles");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUserRoles_UserId",
+                table: "AspNetUserRoles",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "RoleNameIndex",
+                table: "AspNetRoles",
+                column: "NormalizedName");
+        }
+    }
+}
---
+++ AlexWilkinson/Models/Blog.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlexWilkinson.Models
+{
+    public class Blog
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string UrlSlug { get; set; }
+        public string Author { get; set; }
+        public string CoverImg { get; set; }
+
+        [Display(Name = "Content")]
+        public string Body { get; set; }
+
+        public DateTime Created { get; set; }
+
+        [Display(Name = "Publish Date")]
+        public DateTime Published { get; set; }
+
+        public bool Draft { get; set; }
+    }
+}
